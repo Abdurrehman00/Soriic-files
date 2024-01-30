7e233646f8b195c6c68c20d5c68030e0dbd63774
unknown <abdurrehman031011@gmail.com>
2024-01-30 17:33:50 +0500

    services-cards

---
+++ src/components/ServicesList.jsx
@@ -0,0 +1,175 @@
+import React from 'react';
+import './ServicesList.css';
+import img1 from '../assets/service-1.jpg';
+import img2 from '../assets/service-2.jpg';
+// Import other images as needed
+
+const ServicesList = () => {
+    return (
+        <section className="cards-section-main">
+            <div className="services-container">
+                {/* First Service Card */}
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                {/* Second Service Card */}
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                <div className="service-card-container">
+                    <div className="bg-color">
+                    </div>
+                        <div className="main-img-container">
+                            <img src={img1} alt="" />
+                            <span>A</span>
+                        </div>
+                        <div className="content-container-main">
+                            <h2 className="content-heading">Web Development</h2>
+                            <p className="content-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum vero architecto ex commodi fuga, libero praesen</p>
+                        </div>
+                </div>
+
+                {/* Add more service cards as needed */}
+            </div>
+        </section>
+    )
+}
+
+export default ServicesList;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        labelColor: '#5f2dee', // Color for label
+      },
+    },
+  },
+  plugins: [],
+}
+
